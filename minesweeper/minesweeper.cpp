9189c7dd039c056183592b137617f108a8baf57f
coucoul38 <44196664+coucoul38@users.noreply.github.com>
2023-02-22 10:07:26 +0100

    init

--- minesweeper/minesweeper.cpp
+++ minesweeper/minesweeper.cpp
@@ -1,12 +1,90 @@
 // minesweeper.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
 
-#include <iostream>
+//#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    bool lost = false;
+    const int size = 10;
+    int difficulty = 0;
+
+    /* initialisation de la grid */
+    printf("\nSize: %dx%d",size, size);
+    int grid[size][size];
+    /*Counter variables for the loop*/
+    int row, col;
+    for (row = 0; row < size; row++) {
+        for (col = 0; col < size; col++) {
+            grid[row][col] = 0;
+        }
+    }
+    
+    //populate with mines
+    /*printf("\nChoisir la difficulte (entre 0 et 3): ");
+    scanf_s("$d", &difficulty);
+    printf("Difficulty: %d", difficulty);*/
+    int toPlace;
+    switch (difficulty)
+    {
+    case 0:
+        toPlace = 10;
+    default:
+        break;
+    }
+
+
+
+    int placed;
+    printf(" Mines: %d\n", toPlace);
+    //Randomly place mines
+    for (placed = 0; placed < toPlace; placed++) {
+        int rr = rand() % size;
+        int rc = rand() % size;
+        //no duplicates
+        if (grid[rr][rc] == 0) 
+        {
+            grid[rr][rc] = 1;
+            //printf("%d, %d\n", rr, rc);
+        }
+        else {
+            placed = placed - 1;
+        }
+    }
+
+    while (!lost)
+    {   
+        
+        //Displaying array elements
+        //printf("\nLen: %d", len);
+        printf("Two Dimensional array elements:\n");
+        for (row = 0; row < size; row++) {
+            //printf("ROW: %d\n", row);
+            for (col = 0; col < size; col++) {
+                printf("%d ", grid[row][col]);
+                //printf("%d%d ", row, col);
+                if (col == size - 1) {
+                    printf("\n");
+                }
+            }
+        }
+        int inputR, inputC;
+        printf("Entrez les coordonees de la case ainsi: Ligne Colone\nex: 1 0\n");
+        scanf_s("%d %d", &inputR, &inputC);
+        printf("Row: %d, Col: %d", inputR, inputC);
+        
+        //CHECK FOR MINES
+        if (grid[inputR][inputC] == 1) {
+            lost = true;
+            printf("Perdu !");
+        }
+        lost = true;
+    }
+    return 0;
 }
+
+
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
 // Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
